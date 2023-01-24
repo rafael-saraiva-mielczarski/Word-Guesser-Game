63b108080c70096564cc3fd62f0373da74e82bac
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-01-24 10:14:05 -0300

    initial setup

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['Farol', 'Volante', 'Marcha', 'Cinto', 'Assento', 'Motor', 'Porta', 'Capô', 'Pneu', 'Antena'],
+    fruta: ['Manga','Uva','Figo','Amora','Cereja','Banana', 'Maçã', 'Pêra', 'Mamão', 'Laranja'],
+    corpo: ['Abdomem','Ouvido','Orelha','Nariz','Dedos','Braço', 'Perna', 'Pescoço', 'Cabeça', 'Barriga'],
+    computador: ['Memória','Tela','Mouse', 'Teclado', 'Monitor', 'Gabinete'],
+    programação: ['Python','Java','Algoritmos','Cloud','Linguagem', 'FrameWork', 'JavaScript', 'React'],
+    alimento: ['Carne','Ovos','Frango','Peixe','Arroz', 'Feijao', 'Macarrão', 'Acucar', 'Palmito']
+}
