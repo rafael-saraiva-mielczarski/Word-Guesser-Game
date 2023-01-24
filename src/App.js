efd13ff1198ca38904c1d0ea390d515ea6510a34
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-01-24 10:37:53 -0300

    add start screen component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
     <div className="App">
-        <h2>Adivinhe a Palavra</h2>
+        <StartScreen />
     </div>
   );
 }
