9c4b0d8eb996d11b04a2cff7fb915cedb781f32f
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-01-24 10:57:29 -0300

    create all game components, add stage logic to it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import StartScreen from './components/StartScreen';
+import { useCallback, useState, useEffect } from 'react'
+import { wordsList } from './data/words';
+import StartScreen from './components/Start/StartScreen';
+import GameScreen from './components/Game/GameScreen';
+import EndScreen from './components/End/EndScreen';
 
+const stages = [
+    {id: 1, name: "start"},
+    {id: 2, name: "game"},
+    {id: 3, name: "end"}
+]
 function App() {
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsList)
+
   return (
     <div className="App">
-        <StartScreen />
+        {gameStage === "start" && <StartScreen />}
+        {gameStage === "game" && <GameScreen />}
+        {gameStage === "end" && <EndScreen />}
     </div>
   );
 }
---
+++ src/components/Game/GameScreen.js
@@ -0,0 +1,9 @@
+import './GameScreen.css';
+
+const GameScreen = () => {
+  return (
+    <div>GameScreen</div>
+  )
+}
+
+export default GameScreen
